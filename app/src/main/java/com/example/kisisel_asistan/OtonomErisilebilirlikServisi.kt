ad4a2524305fcbf75a325160c42e009a0efc0e70
Erncm <senin-github-mailin@example.com>
2026-09-22 17:15:00 +0300

    Eksik res kaynaklari eklendi ve manifest duzeltildi

--- app/src/main/java/com/example/kisisel_asistan/OtonomErisilebilirlikServisi.kt
+++ app/src/main/java/com/example/kisisel_asistan/OtonomErisilebilirlikServisi.kt
@@ -4,10 +4,6 @@ import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
 
 class OtonomErisilebilirlikServisi : AccessibilityService() {
-
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        // Otomatik tıklama ve mesaj gönderme işlemleri burada yapılacak
-    }
-
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
     override fun onInterrupt() {}
 }
